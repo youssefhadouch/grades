e7900eddb37df45759f62a6070307889ead0b7d7
youssefhadouch <snikrayoussef@gmail.com>
2024-09-08 22:29:33 +0100

    initial commit

---
+++ src/main/java/com/ltp/gradessubmission/grades/Constants.java
@@ -0,0 +1,5 @@
+package com.ltp.gradessubmission.grades;
+
+public class Constants {
+    public static final  int Not_Found = -1000;
+}
---
+++ src/main/java/com/ltp/gradessubmission/grades/Grade.java
@@ -0,0 +1,61 @@
+package com.ltp.gradessubmission.grades;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Grade {
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+    @NotBlank(message = "Subject cannot be blank")
+    private String subject;
+
+    private String score;
+
+
+    public Grade() {
+    }
+
+    public Grade(String name, String subject, String score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "name='" + name + '\'' +
+                ", subject='" + subject + '\'' +
+                ", score='" + score + '\'' +
+                '}';
+    }
+
+
+
+}
